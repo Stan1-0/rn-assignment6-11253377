84dd9a33ed3557407d3046fd2a064c523d1d1fb6
Stanley Boateng <stankofb@gmail.com>
2024-07-01 15:57:18 +0000

    "Added View, TouchableOpacity, and Image components to Checkout screen header, and updated styles accordingly."

--- components/checkoutScreen.js
+++ components/checkoutScreen.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, Image, SafeAreaView, ScrollView } from "react-native";
+import { Text, StyleSheet, Image, SafeAreaView, ScrollView, View, TouchableOpacity } from "react-native";
 
 
 const Checkout = () => {
@@ -6,7 +6,14 @@ const Checkout = () => {
         <SafeAreaView>
             <ScrollView style={styles.scrollViewContainer}>
                 <View style ={styles.container}>
-
+                    <View style={styles.header}>
+                        <Image source={require('../assets/Logo.png')}/>
+                            <View style={styles.icons}>
+                                <TouchableOpacity style={styles.button}>
+                                    <Image source={require('../assets/Search.png')}/>
+                                </TouchableOpacity>
+                            </View>
+                    </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -20,6 +27,18 @@ const styles = StyleSheet.create({
     },
     scrollViewContainer: {
         flexGrow: 1,
+    },
+    header:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: '16',
+    },
+    icons:{
+        flexDirection: 'row',
+    },
+    button: {
+        padding: 8,
     },
 });
 
