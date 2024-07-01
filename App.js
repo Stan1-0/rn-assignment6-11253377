5f7632b6d489c12b11e15d8a32c0a77ed56b1b3f
Stanley Boateng <stankofb@gmail.com>
2024-07-01 10:01:04 +0000

    "Added React Navigation and related dependencies to App.js and package.json"

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { View, StyleSheet} from 'react-native';
 import Home from './components/homeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigatior} from '@react-navigation/stack';
+
+const Stack = createStackNavigatior();
 
 export default function App() {
   return (
-    <View>
-      <Home/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+         name= 'Home'
+         component = {Home}
+         options={{ headerShown: false }}
+         />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
